ee7ba9222154d3d678580567597b627380664d0b
Peter S. Bornerup <pb@onomondo.com>
2023-05-24 15:50:26 +0200

    use custom allocator/free

---
+++ lib/heap_port.c
@@ -0,0 +1,16 @@
+#include <zephyr/kernel.h>
+#include <onomondo/softsim/mem.h>
+
+/**
+ * @brief custom allocator
+ */
+void *port_malloc(size_t size){
+    return k_malloc(size);
+}
+
+/**
+ * @brief custom free
+ */
+void  port_free(void * ptr){
+    k_free(ptr);
+}
--- lib/include/onomondo/softsim/mem.h
+++ lib/include/onomondo/softsim/mem.h
@@ -1,7 +1,22 @@
-#pragma once
+#ifndef MEM_H
+#define MEM_H
 
+#if CONFIG_CUSTOM_HEAP
+
+void *port_malloc(size_t);
+void port_free(void *);
+
+#define SS_ALLOC(obj) port_malloc(sizeof(obj));
+#define SS_ALLOC_N(n) port_malloc(n);
+#define SS_FREE(obj) port_free(obj)
+
+#else // default
 #include <stdlib.h>
 
 #define SS_ALLOC(obj) malloc(sizeof(obj));
 #define SS_ALLOC_N(n) malloc(n);
 #define SS_FREE(obj) free(obj)
+
+#endif // CONFIG_CUSTOM_HEAP
+
+#endif // MEM_H
